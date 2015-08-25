fae55eb6507b3bef7190826a87b6f106cb735930
Andy Stanton <andystanton@gmail.com>
2015-08-25 08:54:01 +0100

    Add component options example.
     * Provide example of how to apply settings at component level.
     * Use @Inject in HttpRoutes & use process() instead of bean() in route.
     * Use two bind calls instead of list->stream->foreach->bind.

--- src/main/java/is/stanton/andy/modules/ApplicationModule.java
+++ src/main/java/is/stanton/andy/modules/ApplicationModule.java
@@ -1,18 +1,28 @@
 package is.stanton.andy.modules;
 
+import com.google.inject.Provides;
 import is.stanton.andy.processors.PingEndpoint;
 import is.stanton.andy.routes.HttpRoutes;
+import org.apache.camel.component.jetty.JettyHttpComponent;
+import org.apache.camel.component.jetty8.JettyHttpComponent8;
 import org.apache.camel.guice.CamelModuleWithMatchingRoutes;
-
-import java.util.Arrays;
+import org.apache.camel.guice.jndi.JndiBind;
 
 public class ApplicationModule extends CamelModuleWithMatchingRoutes {
     @Override
     protected void configure() {
         super.configure();
-        Arrays.asList(
-                PingEndpoint.class,
-                HttpRoutes.class
-        ).stream().forEach(this::bind);
+
+        bind(PingEndpoint.class);
+        bind(HttpRoutes.class);
+    }
+
+    @Provides
+    @JndiBind("jetty")
+    private JettyHttpComponent jettyComponent() {
+        JettyHttpComponent component = new JettyHttpComponent8();
+        component.setMinThreads(10);
+        component.setMaxThreads(20);
+        return component;
     }
 }
--- src/main/java/is/stanton/andy/routes/HttpRoutes.java
+++ src/main/java/is/stanton/andy/routes/HttpRoutes.java
@@ -3,13 +3,23 @@ package is.stanton.andy.routes;
 import is.stanton.andy.processors.PingEndpoint;
 import org.apache.camel.builder.RouteBuilder;
 
+import javax.inject.Inject;
+
 public class HttpRoutes extends RouteBuilder {
     public static final String URI_PATTERN = "%s:http://%s:%d%s?%s";
     public static final String COMPONENT_NAME = "jetty";
     public static final String HOST = "0.0.0.0";
     public static final int PORT = 8080;
+
     public static final String PING_ENDPOINT = "/ping";
     public static final String PING_OPTIONS = "chunked=false";
+
+    private PingEndpoint pingEndpoint;
+
+    @Inject
+    public HttpRoutes(PingEndpoint pingEndpoint) {
+        this.pingEndpoint = pingEndpoint;
+    }
 
     @Override
     public void configure() throws Exception {
@@ -20,6 +30,6 @@ public class HttpRoutes extends RouteBuilder {
                         HttpRoutes.PORT,
                         HttpRoutes.PING_ENDPOINT,
                         HttpRoutes.PING_OPTIONS)
-        ).bean(PingEndpoint.class);
+        ).process(pingEndpoint);
     }
 }
