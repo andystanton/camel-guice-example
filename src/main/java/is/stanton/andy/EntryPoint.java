13468f6ac24975557f669657357b2a8e1c76eed4
Andy Stanton <andystanton@gmail.com>
2015-08-26 12:44:12 +0100

    Remove unnecessary Guice.createInjector() from main

--- src/main/java/is/stanton/andy/EntryPoint.java
+++ src/main/java/is/stanton/andy/EntryPoint.java
@@ -1,13 +1,10 @@
 package is.stanton.andy;
 
-import com.google.inject.Guice;
-
 import javax.naming.InitialContext;
 
 public class EntryPoint {
     public static void main(String[] args) throws Exception {
         new InitialContext();
-        Guice.createInjector();
 
         System.out.println("Running Camel + Guice example on http://127.0.0.1:8080/ping");
         System.out.println("Press Ctrl-C to quit");
