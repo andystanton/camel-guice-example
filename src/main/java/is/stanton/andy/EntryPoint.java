ea91ec6b4c6070525a0117b735795efa5d4f3bf4
Andy Stanton <andystanton@gmail.com>
2015-08-26 10:59:52 +0100

    Lower logging level as this is example code

--- src/main/java/is/stanton/andy/EntryPoint.java
+++ src/main/java/is/stanton/andy/EntryPoint.java
@@ -1,19 +1,15 @@
 package is.stanton.andy;
 
 import com.google.inject.Guice;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.naming.InitialContext;
 
 public class EntryPoint {
-    private static Logger log = LoggerFactory.getLogger(EntryPoint.class);
-
     public static void main(String[] args) throws Exception {
         new InitialContext();
         Guice.createInjector();
 
-        log.info("Running Camel + Guice example!");
-        log.info("Press Ctrl-C to quit");
+        System.out.println("Running Camel + Guice example on http://127.0.0.1:8080/ping");
+        System.out.println("Press Ctrl-C to quit");
     }
 }
